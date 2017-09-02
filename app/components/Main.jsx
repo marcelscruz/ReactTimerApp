f95e82f49bb887f248e9206a057ae853b0edb340
Marcel Cruz <marcelcruz@live.com>
2017-09-02 17:23:30 +0100

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <Nav/>
+          <p>Timer App</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
